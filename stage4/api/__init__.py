0232c8f462e1418ed68202cfa29414c4c72a2f66
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-10-26 15:05:58 +0000

    :sparkles: added the creation and initialization of the flask application and also included registration of blueprints

--- stage4/api/__init__.py
+++ stage4/api/__init__.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from api.models import Library
+from flask import Flask
 """
 Module that acts as base for other modules.
 """
 
 
+app = Flask(__name__)
 engine = create_engine("sqlite:///library.db")
 Library.metadata.create_all(engine)
 session = Session(engine)
+
+from api.routes import library
+app.register_blueprint(library)
