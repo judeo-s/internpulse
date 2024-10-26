218fd531e62802268b8815b4fed976ce169854ea
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-10-26 17:49:32 +0000

    :adhesive_bandage: made a few changes

--- stage4/api/__init__.py
+++ stage4/api/__init__.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from api.models import Library
+from api.v1.models import Library
 from flask import Flask
 """
 Module that acts as base for other modules.
@@ -8,9 +8,16 @@ Module that acts as base for other modules.
 
 
 app = Flask(__name__)
-engine = create_engine("sqlite:///library.db")
+engine = create_engine("sqlite:///data/library.db")
 Library.metadata.create_all(engine)
 session = Session(engine)
 
-from api.routes import library
-app.register_blueprint(library)
+
+from api.v1.routes import library_v1
+"""
+Register the blueprint for versioned routes
+"""
+
+
+app.register_blueprint(library_v1)
+app.register_blueprint(library_v1)
